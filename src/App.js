acae035875e37383758e2f174b23c879a4e49041
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-10-17 11:19:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ class App extends Component {
               path="/Post-Manager-App-with-Redux/Edit/:postID"
               render={props => <Edit {...props} />}
             />
-            {/* <Route path="/Footer" component = {Footer} /> */}
           </Switch>
         </Router>
         
